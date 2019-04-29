12c0f612e235dc15ab8a4fb7d6313bab91608c4b
wingled22 <pelayowindel@gmail.com>
2019-04-29 22:12:11 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<div id="container">
+		<menu>
+			<a href="students.php">
+				<button>Student</button>
+			</a>
+			
+			<a href="">
+				<button>Option 2</button>
+			</a>
+			
+			<a href="">
+				<button>Option 3</button>
+			</a>
+		</menu>
+	</div>
+</body>
+</html>
---
+++ students.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<div id="container">
+
+		<form>
+			<input type="text" name="firstname">
+			<input type="text" name="lastname">
+			<select name="course">
+				<option value="BSIT">BSIT</option>
+				<option value="BSED">BSED</option>
+				<option value="BEED">BEED</option>
+				<option value="BSCrim">BSCrim</option>
+			</select>
+			<input type="submit" name="">
+		</form>
+		<br>
+		<table>
+			<thead>
+				<th>head</th>
+				<th>head</th>
+				<th>head</th>
+				<th>Action</th>
+			</thead>
+			<tbody>
+				<tr>
+					<td>t=asdf</td>
+					<td>t=asdf</td>
+					<td>t=asdf</td>
+					<td>update || delete</td>
+				</tr>
+
+				<tr>
+					<td>the brown fox</td>
+					<td>the brown fox</td>
+					<td>the brown fox</td>
+					<td>update || delete</td>
+
+				</tr>
+
+				<tr>
+					<td>jumps the lazy dog	</td>
+					<td>jumps the lazy dog	</td>
+					<td>jumps the lazy dog	</td>
+					<td>update || delete</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
